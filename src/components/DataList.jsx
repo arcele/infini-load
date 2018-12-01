216946441d6b3a306389ae4159560b796bc385e4
Ryan Kelley <ryan.kelley@espn.com>
2018-12-01 11:21:44 -0500

    fetching from api, saving to local state

--- src/components/DataList.jsx
+++ src/components/DataList.jsx
@@ -7,25 +7,37 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import CircularProgress from 'material-ui/CircularProgress';
 
 class DataList extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      fetching: false,
+      projects: []
+    }
+  }
+
   componentDidMount() {
-    this.fetched = 0
-    this.fetching = false
-    
-    console.log('fetch stuff from : https://portfolium.com/proxy/entries/expert?limit=13&offset=0&sort=recent')
+    this.loadItems()
+  }
+
+  loadItems() {
     const limit = 15
-    var uri = `https://portfolium.com/proxy/entries/expert?limit=${limit}&offset=${this.fetched}&sort=recent`
+    var uri = `https://portfolium.com/proxy/entries/expert?limit=${limit}&offset=${this.state.projects.length}&sort=recent`
+    this.setState({fetching:true})
     fetch(uri)
       .then(res => res.json())
       .then((projects) => {
         console.log('heres projects:', projects)
-      })
-
+        this.setState({projects:projects, fetching: false})
+      })    
   }
 
+
   render() {
+
     return(
         <Table>
           <TableHeader>
@@ -36,11 +48,22 @@ class DataList extends Component {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableRowColumn>?</TableRowColumn>
-              <TableRowColumn>Descriptive Title</TableRowColumn>
-              <TableRowColumn>Detailed Description</TableRowColumn>
-            </TableRow>
+            { this.state.projects.map(project => (
+                <TableRow key={project.id}>
+                  <TableRowColumn>?</TableRowColumn>
+                  <TableRowColumn>{project.title}</TableRowColumn>
+                  <TableRowColumn>{project.description}</TableRowColumn>
+                </TableRow>
+              )) 
+            }
+            { this.state.fetching && 
+              <TableRow key='loading'>
+                <TableRowColumn>&nbsp;</TableRowColumn>
+                <TableRowColumn>
+                  <CircularProgress size={50} thickness={5} />
+                </TableRowColumn>
+              </TableRow>
+            }
           </TableBody>
         </Table>
     )
